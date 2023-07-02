412882e91bc923bcac47edea5b5301424eaa8836
Levi9111 <captainlevi9111@gmail.com>
2023-07-02 09:55:39 +0600

    resume added

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,5 +1,4 @@
 import SectionTitle from "../../Utils/SectionTitle/SectionTitle";
-import Table from "../Table/Table";
 import "./Resume.css";
 
 const Resume = () => {
@@ -7,19 +6,49 @@ const Resume = () => {
     <section id="resume">
       <SectionTitle heading="Summary" subHeading="Resume" />
       {/* education */}
-      <div className="education">
-        <h4>My Education</h4>
-        <div className="">
-          <span className="year">2020</span>
-          <p>Chittagong College, Chattogram, Bangladesh</p>
+      <div className="px-6 md:max-w-6xl mx-auto ">
+        <div className="education">
+          <h4 className="text-2xl font-semibold text-white mb-3">
+            My Education
+          </h4>
+          <div className="education-container bg-black bg-opacity-30">
+            <span className="year">2020</span>
+            <h3 className="text-2xl text-white font-semibold mt-2">HSC</h3>
+            <p className="resume-text">
+              Chittagong College, <span>Chattogram, Bangladesh</span>
+            </p>
+          </div>
         </div>
-      </div>
 
-      {/* courses */}
-      <div className="courses">
-        <h4>My Courses</h4>
-        <div className="w-full">
-          <Table />
+        {/* courses */}
+        <div className="courses">
+          <h4 className="text-2xl font-semibold text-white my-3">My Courses</h4>
+          <div className="courses-container">
+            <table className="courses-table">
+              <tr>
+                <td>Advance CSS and SASS</td>
+                <td>Jonas Schmedtmann</td>
+                <td>Udemy</td>
+              </tr>
+              <tr>
+                <td>Complete JavaScript Course</td>
+                <td>Jonas Schmedtmann</td>
+                <td>Udemy</td>
+              </tr>
+              <tr>
+                <td>Complete Web Development Dootcamp</td>
+                <td>Angela Yu</td>
+                <td>Udemy</td>
+              </tr>
+              <tr>
+                <td>
+                  Complete Web Development <br /> with Jhankar Mahbub
+                </td>
+                <td>Jhankar Mahbub</td>
+                <td>Programming Hero</td>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
     </section>
